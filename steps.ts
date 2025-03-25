43607a00bab632aaa118508469d9326d1793f963
subas-roy <subasroy46@gmail.com>
2025-03-26 00:52:38 +0600

    React installation

---
+++ steps.ts
@@ -0,0 +1,10 @@
+/**
+ * Basic Installations
+ *    1. npm create vite
+ *    2. framework: React
+ *    3. variant: Typescript
+ *    4. npm i
+ *    5. npm i react-router-dom
+ *    6. npm i react-hook-form
+ *    7. npm i antd
+ */
