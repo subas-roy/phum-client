4df994621ef5da0a95f7d446ccbf7c61911a412c
subas-roy <subasroy46@gmail.com>
2025-03-26 21:54:45 +0600

    Added about and contact as relative paths, register and login as absolute paths

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -1,10 +1,32 @@
 import App from '../App';
 import { createBrowserRouter } from 'react-router-dom';
+import About from '../pages/About';
+import Contact from '../pages/Contact';
+import Register from '../pages/Register';
+import Login from '../pages/Login';
 
 const router = createBrowserRouter([
   {
     path: '/',
     element: <App />,
+    children: [
+      {
+        path: 'about', // relative path
+        element: <About />,
+      },
+      {
+        path: 'contact',
+        element: <Contact />,
+      },
+    ],
+  },
+  {
+    path: '/register', // absolute path
+    element: <Register />,
+  },
+  {
+    path: '/login',
+    element: <Login />,
   },
 ]);
 
