b70fe78f11ef89d2a8ac9e59ccfce5843ab5fed2
subas-roy <subasroy46@gmail.com>
2025-03-26 21:19:51 +0600

    Basic router setup

--- steps.ts
+++ steps.ts
@@ -1,5 +1,7 @@
 /**
+ * --------------------
  * Basic Installations
+ * ---------------------
  *    1. npm create vite
  *    2. framework: React
  *    3. variant: Typescript
@@ -7,4 +9,9 @@
  *    5. npm i react-router-dom
  *    6. npm i react-hook-form
  *    7. npm i antd
+ *
+ * -----------------------------
+ * Routing paths (role based)
+ * -----------------------------
+ *    www.maindomain/admin/create-stuent
  */
