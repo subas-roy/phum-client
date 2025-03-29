9ac208fef304f42af4434344ee510cd66dfffb3c
subas-roy <subasroy46@gmail.com>
2025-03-29 09:25:59 +0600

    Import admin's nested/children paths from admin routes

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -4,10 +4,7 @@ import About from '../pages/About';
 import Contact from '../pages/Contact';
 import Register from '../pages/Register';
 import Login from '../pages/Login';
-import AdminDashboard from '../pages/admin/AdminDashboard';
-import CreateStudent from '../pages/admin/CreateStudent';
-import CreateAdmin from '../pages/admin/CreateAdmin';
-import CreateFaculty from '../pages/admin/CreateFaculty';
+import { adminPaths } from './admin.routes';
 
 const router = createBrowserRouter([
   {
@@ -27,28 +24,7 @@ const router = createBrowserRouter([
   {
     path: '/admin',
     element: <App />,
-    children: [
-      {
-        index: true,
-        element: <AdminDashboard />,
-      },
-      {
-        path: 'dashboard',
-        element: <AdminDashboard />,
-      },
-      {
-        path: 'create-admin',
-        element: <CreateAdmin />,
-      },
-      {
-        path: 'create-faculty',
-        element: <CreateFaculty />,
-      },
-      {
-        path: 'create-student',
-        element: <CreateStudent />,
-      },
-    ],
+    children: adminPaths,
   },
   {
     path: '/register', // absolute path
