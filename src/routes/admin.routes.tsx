aa2cb5daac5ab41fff7598b295e2a97c9983cb43
subas-roy <subasroy46@gmail.com>
2025-03-30 17:04:38 +0600

    Refactor hard-coded sidebar items

--- src/routes/admin.routes.tsx
+++ src/routes/admin.routes.tsx
@@ -3,10 +3,17 @@ import AdminDashboard from '../pages/admin/AdminDashboard';
 import CreateAdmin from '../pages/admin/CreateAdmin';
 import CreateFaculty from '../pages/admin/CreateFaculty';
 import CreateStudent from '../pages/admin/CreateStudent';
+import { NavLink } from 'react-router-dom';
 
 type TRoute = {
   path: string;
   element: ReactNode;
+};
+
+type TSidebarItem = {
+  key: string;
+  label: ReactNode;
+  children?: TSidebarItem[];
 };
 
 // A centralised array for routes and sidebar items (like union)
@@ -37,6 +44,31 @@ export const adminPaths = [
     ],
   },
 ];
+
+export const adminSidebarItems = adminPaths.reduce(
+  (acc: TSidebarItem[], item) => {
+    if (item.path && item.element) {
+      acc.push({
+        key: item.name,
+        label: <NavLink to={`/admin/${item.path}`}>{item.name}</NavLink>,
+      });
+    }
+
+    if (item.children) {
+      acc.push({
+        key: item.name,
+        label: item.name,
+        children: item.children.map((child) => ({
+          key: child.name,
+          label: <NavLink to={`/admin/${child.path}`}>{child.name}</NavLink>,
+        })),
+      });
+    }
+
+    return acc;
+  },
+  []
+);
 
 //* Programmatical way
 
