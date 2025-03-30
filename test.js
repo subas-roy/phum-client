94dfc07902d8a2b0f7712897492801be1d29998a
subas-roy <subasroy46@gmail.com>
2025-03-30 12:18:55 +0600

    Return an array of object with key and label using reduce() and if-else

--- test.js
+++ test.js
@@ -38,19 +38,20 @@ const adminPaths2 = [
 const newArray = adminPaths2.reduce((acc, item) => {
   if (item.path && item.element) {
     acc.push({
-      path: item.path,
-      element: item.element,
+      key: item.name,
+      label: 'Navlink',
     });
   }
 
-  if (item.children) {
-    item.children.forEach((child) => {
-      acc.push({
-        path: child.path,
-        element: child.element,
-      });
-    });
-  }
+  // if (item.children) {
+  //   item.children.forEach((child) => {
+  //     acc.push({
+  //       path: child.path,
+  //       element: child.element,
+  //     });
+  //   });
+  // }
+
   return acc;
 }, []);
 
