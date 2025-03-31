0e1b5bed539bc45d45a08757f0d3cf8d24a3da79
subas-roy <subasroy46@gmail.com>
2025-04-01 03:27:11 +0600

    Added the generated slice reducer to store from baseApi

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,10 +1,17 @@
 import { configureStore } from '@reduxjs/toolkit';
 import authReducer from './features/auth/authSlice';
+import { baseApi } from './api/baseApi';
 
 export const store = configureStore({
   reducer: {
+    // Add the generated reducer as a specific top-level slice
+    [baseApi.reducerPath]: baseApi.reducer, // baseApi: baseApi.reducer
     auth: authReducer,
   },
+  // Adding the api middleware enables caching, invalidation, polling,
+  // and other useful features of `rtk-query`.
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(baseApi.middleware),
 });
 
 // Infer the `RootState` and `AppDispatch` types from the store itself
