591ac542f781671c492bf9e314751a2f9cfed316
subas-roy <subasroy46@gmail.com>
2025-03-31 11:02:05 +0600

    Separate Sider as Sidebar from MainLaout

--- src/components/layout/MainLayout.tsx
+++ src/components/layout/MainLayout.tsx
@@ -1,40 +1,12 @@
-import { Layout, Menu } from 'antd';
+import { Layout } from 'antd';
 import { Outlet } from 'react-router-dom';
-import { sidebarItemsGenerator } from '../../utils/sidebarItemsGenerator';
-import { adminPaths } from '../../routes/admin.routes';
-const { Content, Footer, Header, Sider } = Layout;
+import Sidebar from './Sidebar';
+const { Content, Footer, Header } = Layout;
 
 const MainLayout = () => {
   return (
     <Layout style={{ height: '100vh' }}>
-      <Sider
-        breakpoint="lg"
-        collapsedWidth="0"
-        onBreakpoint={(broken) => {
-          console.log(broken);
-        }}
-        onCollapse={(collapsed, type) => {
-          console.log(collapsed, type);
-        }}
-      >
-        <div
-          style={{
-            color: 'white',
-            height: '4rem',
-            display: 'flex',
-            justifyContent: 'center',
-            alignItems: 'center',
-          }}
-        >
-          <h1>Ph University</h1>
-        </div>
-        <Menu
-          theme="dark"
-          mode="inline"
-          defaultSelectedKeys={['4']}
-          items={sidebarItemsGenerator(adminPaths, 'admin')}
-        />
-      </Sider>
+      <Sidebar />
       <Layout>
         <Header style={{ padding: 0 }} />
         <Content style={{ margin: '24px 16px 0' }}>
