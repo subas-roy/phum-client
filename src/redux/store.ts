2d2b9b79adc44c91d394a0f17784118da98674ee
subas-roy <subasroy46@gmail.com>
2025-04-01 17:30:28 +0600

    Integrate redux persist to persist data in local storage and get after reloading

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,12 +1,21 @@
 import { configureStore } from '@reduxjs/toolkit';
 import authReducer from './features/auth/authSlice';
 import { baseApi } from './api/baseApi';
+import { persistReducer, persistStore } from 'redux-persist';
+import storage from 'redux-persist/lib/storage'; // defaults to localStorage for web
+
+const persistConfig = {
+  key: 'auth',
+  storage,
+};
+
+const persistedAuthReducer = persistReducer(persistConfig, authReducer);
 
 export const store = configureStore({
   reducer: {
     // Add the generated reducer as a specific top-level slice
     [baseApi.reducerPath]: baseApi.reducer, // baseApi: baseApi.reducer
-    auth: authReducer,
+    auth: persistedAuthReducer,
   },
   // Adding the api middleware enables caching, invalidation, polling,
   // and other useful features of `rtk-query`.
@@ -18,3 +27,5 @@ export const store = configureStore({
 export type RootState = ReturnType<typeof store.getState>;
 // Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
 export type AppDispatch = typeof store.dispatch;
+
+export const persistor = persistStore(store);
