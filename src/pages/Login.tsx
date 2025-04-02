94b2f0a8bff12089032b2dfcbbf5cc87efed6869
subas-roy <subasroy46@gmail.com>
2025-04-02 07:03:32 +0600

    Added FieldValues from 'react-hook-form'

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,5 +1,5 @@
 import { Button } from 'antd';
-import { useForm } from 'react-hook-form';
+import { FieldValues, useForm } from 'react-hook-form';
 import { useLoginMutation } from '../redux/features/auth/authApi';
 import { useAppDispatch } from '../redux/hooks';
 import { setUser, TUser } from '../redux/features/auth/authSlice';
@@ -18,9 +18,9 @@ const Login = () => {
     },
   });
 
-  const [login, { error }] = useLoginMutation();
+  const [login] = useLoginMutation();
 
-  const onSubmit = async (data) => {
+  const onSubmit = async (data: FieldValues) => {
     const toastId = toast.loading('Logging in');
 
     try {
