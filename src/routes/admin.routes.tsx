031862bbbc078ac8f7525d398b2d092d3922e9b4
subas-roy <subasroy46@gmail.com>
2025-04-02 09:33:14 +0600

    Added academic management path in adminPaths

--- src/routes/admin.routes.tsx
+++ src/routes/admin.routes.tsx
@@ -1,3 +1,4 @@
+import AcademicSemester from '../pages/admin/academicManagement/AcademicSemester';
 import AdminDashboard from '../pages/admin/AdminDashboard';
 import CreateAdmin from '../pages/admin/CreateAdmin';
 import CreateFaculty from '../pages/admin/CreateFaculty';
@@ -9,6 +10,16 @@ export const adminPaths = [
     name: 'Dashboard',
     path: 'dashboard',
     element: <AdminDashboard />,
+  },
+  {
+    name: 'Academic Management',
+    children: [
+      {
+        name: 'Academic Semesters',
+        path: 'academic-semesters',
+        element: <AcademicSemester />,
+      },
+    ],
   },
   {
     name: 'User Management',
