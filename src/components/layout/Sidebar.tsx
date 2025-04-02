797314f2ce208983dee94ed28cb6cd6ddbe3a52a
subas-roy <subasroy46@gmail.com>
2025-04-02 09:29:39 +0600

    Dynamic current user in Sidebar

--- src/components/layout/Sidebar.tsx
+++ src/components/layout/Sidebar.tsx
@@ -3,6 +3,8 @@ import { sidebarItemsGenerator } from '../../utils/sidebarItemsGenerator';
 import { adminPaths } from '../../routes/admin.routes';
 import { facultyPaths } from '../../routes/faculty.routes';
 import { studentPaths } from '../../routes/student.routes';
+import { useAppSelector } from '../../redux/hooks';
+import { selectCurrentUser } from '../../redux/features/auth/authSlice';
 
 const { Sider } = Layout;
 
@@ -13,10 +15,10 @@ const userRole = {
 };
 
 const Sidebar = () => {
-  const role = 'faculty'; // Change the role
+  const user = useAppSelector(selectCurrentUser);
   let sidebarItems;
 
-  switch (role) {
+  switch (user!.role) {
     case userRole.ADMIN:
       sidebarItems = sidebarItemsGenerator(adminPaths, userRole.ADMIN);
       break;
