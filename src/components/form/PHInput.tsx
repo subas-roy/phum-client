562f3673d892fd4d0cf17e341895579f36b64ba9
subas-roy <subasroy46@gmail.com>
2025-04-30 13:39:40 +0600

    React Hook Form controller for 3rd party input binding

--- src/components/form/PHInput.tsx
+++ src/components/form/PHInput.tsx
@@ -1,12 +1,15 @@
-import { useFormContext } from 'react-hook-form';
+import { Input } from 'antd';
+import { Controller } from 'react-hook-form';
 
 const PHInput = ({ type, name, label }) => {
-  const { register } = useFormContext();
   return (
-    <>
+    <div style={{ marginBottom: '20px' }}>
       {label ? label : null}
-      <input type={type} id={name} {...register(name)} />
-    </>
+      <Controller
+        name={name}
+        render={({ field }) => <Input {...field} type={type} id={name} />}
+      />
+    </div>
   );
 };
 
--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,4 +1,4 @@
-import { Button } from 'antd';
+import { Button, Row } from 'antd';
 import { FieldValues, useForm } from 'react-hook-form';
 import { useLoginMutation } from '../redux/features/auth/authApi';
 import { useAppDispatch } from '../redux/hooks';
@@ -24,33 +24,31 @@ const Login = () => {
 
   const onSubmit = async (data: FieldValues) => {
     console.log(data);
-    // const toastId = toast.loading('Logging in');
+    const toastId = toast.loading('Logging in');
 
-    // try {
-    //   const userInfo = {
-    //     id: data.id,
-    //     password: data.password,
-    //   };
-    //   const res = await login(userInfo).unwrap(); // send data to useLoginMutation
-    //   const user = verifyToken(res.data.accessToken) as TUser;
-    //   dispatch(setUser({ user: user, token: res.data.accessToken }));
-    //   toast.success('Logged in', { id: toastId, duration: 2000 });
-    //   navigate(`/${user.role}/dashboard`);
-    // } catch (error) {
-    //   toast.error('Something went wrong', { id: toastId, duration: 2000 });
-    // }
+    try {
+      const userInfo = {
+        id: data.id,
+        password: data.password,
+      };
+      const res = await login(userInfo).unwrap(); // send data to useLoginMutation
+      const user = verifyToken(res.data.accessToken) as TUser;
+      dispatch(setUser({ user: user, token: res.data.accessToken }));
+      toast.success('Logged in', { id: toastId, duration: 2000 });
+      navigate(`/${user.role}/dashboard`);
+    } catch (error) {
+      toast.error('Something went wrong', { id: toastId, duration: 2000 });
+    }
   };
 
   return (
-    <PHForm onSubmit={onSubmit}>
-      <div>
-        <PHInput type="text" name="userId" label="ID: " />
-      </div>
-      <div>
+    <Row justify="center" align="middle" style={{ height: '100vh' }}>
+      <PHForm onSubmit={onSubmit}>
+        <PHInput type="text" name="id" label="ID: " />
         <PHInput type="text" name="password" label="Password: " />
-      </div>
-      <Button htmlType="submit">Login</Button>
-    </PHForm>
+        <Button htmlType="submit">Login</Button>
+      </PHForm>
+    </Row>
   );
 };
 
